56a5620c46aa4649b660a39b5ceef0b1ae9c7304
AmandaZerpa <amanditazerpa@gmail.com>
2021-10-18 21:47:31 -0300

    añado archivo procfile

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.get('/register',(req,res)=>{
 app.get('/login',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/login.html'))
 })
-app.listen(3000,()=>{
-    console.log("El servidor está levantado")
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("Servidor corriendo en el puerto 3000")
 })
